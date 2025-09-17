ea95bf8c220ec496fbd4d44e06ebc96f9eef8608
Lyndon <lynndon@gmail.com>
2025-09-17 10:06:44 +0800

    refactor: use RustCrypto blake2
    binary ckb-dao-vote: 102744 -> 110384
    cycles: 1914878 -> 1832273

--- contracts/ckb-dao-vote/src/smt_hasher.rs
+++ contracts/ckb-dao-vote/src/smt_hasher.rs
@@ -1,18 +1,14 @@
-use blake2b_ref::{Blake2b, Blake2bBuilder};
+use blake2::digest::{CustomizedInit, FixedOutput};
+use blake2::{Blake2b256, Digest};
 use sparse_merkle_tree::{H256, traits::Hasher};
 
-const BLAKE2B_KEY: &[u8] = &[];
-const BLAKE2B_LEN: usize = 32;
 const PERSONALIZATION: &[u8] = b"ckb-default-hash";
 
-pub struct Blake2bHasher(Blake2b);
+pub struct Blake2bHasher(Blake2b256);
 
 impl Default for Blake2bHasher {
     fn default() -> Self {
-        let blake2b = Blake2bBuilder::new(BLAKE2B_LEN)
-            .personal(PERSONALIZATION)
-            .key(BLAKE2B_KEY)
-            .build();
+        let blake2b = Blake2b256::new_customized(PERSONALIZATION);
         Blake2bHasher(blake2b)
     }
 }
@@ -22,11 +18,10 @@ impl Hasher for Blake2bHasher {
         self.0.update(h.as_slice());
     }
     fn write_byte(&mut self, b: u8) {
-        self.0.update(&[b][..]);
+        self.0.update([b]);
     }
     fn finish(self) -> H256 {
-        let mut hash = [0u8; 32];
-        self.0.finalize(&mut hash);
-        hash.into()
+        let result: [u8; 32] = self.0.finalize_fixed().as_slice().try_into().unwrap();
+        result.into()
     }
 }
